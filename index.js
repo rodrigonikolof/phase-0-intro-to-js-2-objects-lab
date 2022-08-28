9d5dc494b8a2666af50f3844afdb4ffb97361b03
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-08-28 19:16:07 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+// class employee {
+
+//     // constructor (name, streetAddress){
+//     //     this.name = name;
+//     //     this.streetAddress = streetAddress;
+//     //     }
+//     // }
+    
+//     // const employee1 = new employee ("Obi-Wan", "66 Death Star Road");
+    
+//     // console.log(employee1);
+
+
+
+const employee = {};
+
+employee.name = "Obi-Wan Kenobi";
+employee.streetAddress = "66 Death Star Road";
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+ ...obj, [key] : value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+     obj [key] = value;
+     return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj};
+    delete obj.key;
+    return {newEmployee} 
+    }
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    const newEmployee = {obj};
+    delete obj[key];
+    return (employee);
+}
+
